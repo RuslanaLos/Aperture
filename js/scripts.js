567bf546d6356c3f2dfed90ceb1e2876f1b2f8e7
RuslanaLos <ruslanalos74@gmail.com>
2023-06-22 19:08:54 +0300

    scss

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,10 @@ $(document).ready(function(){
             280:{
                 items:1
             },
-            1000:{
+            768:{
+                items:4
+            },
+            992:{
                 items:5
             }
         }
@@ -33,6 +36,46 @@ $(document).ready(function(){
     $( "#accordion" ).accordion({
         icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
     });
+
+
+    (function($) { "use strict";
+
+        var app = function () {
+            var body = undefined;
+            var menu = undefined;
+            var menuItems = undefined;
+            var init = function init() {
+                body = document.querySelector('body');
+                menu = document.querySelector('.menu-icon');
+                menuItems = document.querySelectorAll('.nav__list-item');
+                applyListeners();
+            };
+            var applyListeners = function applyListeners() {
+                menu.addEventListener('click', function () {
+                    return toggleClass(body, 'nav-active');
+                });
+            };
+            var toggleClass = function toggleClass(element, stringClass) {
+                if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+            };
+            init();
+        }();
+
+
+        //Switch light/dark
+
+        $("#switch").on('click', function () {
+            if ($("body").hasClass("light")) {
+                $("body").removeClass("light");
+                $("#switch").removeClass("switched");
+            }
+            else {
+                $("body").addClass("light");
+                $("#switch").addClass("switched");
+            }
+        });
+
+    })(jQuery);
 
 
 
